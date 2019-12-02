afce565608d4a44c9c3d54cfb7801d64e4401d26
Scott Raigner <scott.raigner@realwear.com>
2019-12-02 14:07:15 -0800

    main.tf

--- terransible/variables.tf
+++ terransible/variables.tf
@@ -1,20 +1,20 @@
 variable "aws_region" {}
 variable "aws_profile" {}
-#data "aws_availability_zones" "available" {}
-#variable "localip" {}
-#variable "vpc_cidr" {}
-#
-#variable "cidrs" {
-#  type = "map"
-#}
-#
+data "aws_availability_zones" "available" {}
+variable "localip" {}
+variable "vpc_cidr" {}
+
+variable "cidrs" {
+  type = "map"
+}
+
 #variable "db_instance_class" {}
 #variable "dbname" {}
 #variable "dbuser" {}
 #variable "dbpassword" {}
 #variable "key_name" {}
 #variable "public_key_path" {}
-#variable "domain_name" {}
+variable "domain_name" {}
 #variable "dev_instance_type" {}
 #variable "dev_ami" {}
 #variable "elb_healthy_threshold" {}
